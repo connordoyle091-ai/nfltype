acaf813456749bd2ae50c728b7a1e4031cd1f046
connordoyle091-ai <connordoyle091@gmail.com>
2026-01-01 00:51:02 -0500

    button

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ button.addEventListener("click", whenButtonClicked);
 function whenButtonClicked () {
     console.log("button was clicked")
 }
+function timesButtonClicked(nums) {
+    if(nums > 10){
+        console.log("Quit it")
+    }
+}
+timesButtonClicked(whenButtonClicked())
