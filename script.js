faf1935a6fa7ed46c607d5ad75ec962b2be653be
connordoyle091-ai <connordoyle091@gmail.com>
2026-01-04 20:46:45 -0500

    snow day tommorow please

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 const btn = document.getElementById("myButton");
 btn.addEventListener("click", whenButtonClicked);
-let count = 0
 function whenButtonClicked () {
-    count ++
-    console.log("button was clicked")
-    if(count > 10){
-        console.log("Quit it")
-    }
+    btn.style
 }
